526cf2b207da94a57662e3298be47c6c752e3d83
Dan Corley <dcorle2@gmail.com>
2021-06-23 16:36:28 -0500

    improve threading and code cleanup

--- lyric_scraper.py
+++ lyric_scraper.py
@@ -1,14 +1,20 @@
-import time
+# selenium and BeautifulSoup for pulling info
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from bs4 import BeautifulSoup
+
+# sentiment analysis
 from textblob import TextBlob
-import requests, json, collections, os
-import numpy as np
+
+import time
+import requests
+import os
+import re
 import pandas as pd
-import string
-from multiprocessing import Pool
-import boto3
+import multiprocess as mp
 
 
 class Artist:
@@ -23,41 +29,75 @@ class Artist:
         .get_lyrics() - return lyrics from all songs
     '''
     
-    def __init__(self, name):
+    def __init__(self, name, driver_path='webdrivers/chromedriver'):
         self.name = name
         self.albums = 'none yet. run get_albums'
         self.songs = 'none yet. run get_songs'
         self.lyrics = 'none yet. run_get lyrics'
-    
-    def get_albums(self):
+        try:
+            self.path = os.path.dirname(__file__)
+        except NameError:
+            # file path working in Jupyter
+            self.path = os.getcwd()
+            
+        self.chromedriver_path = os.path.join(self.path, driver_path)
+        
+        
+    def get_albums(self, headless=True):
         '''
         input: artist's name 
-        output: 
+        output: None, .albums
         '''
+        
+        # if you'd like the scraping to be visible
+        chrome_options = webdriver.chrome.options.Options()
+        chrome_options.headless = headless
+        
         # initialize the selenium driver and open the artist's page
-        driver = webdriver.Chrome('/Users/dcorley/Documents/Flatiron/Lyrics/lyric_scraper/chromedriver')
+        try:
+            driver = webdriver.Chrome(
+                executable_path = self.chromedriver_path,
+                options = chrome_options
+            )
+        except Exception as e:
+            ns = 'this version of chromedriver is not supported, go get your supported version'
+            dl = 'https://chromedriver.chromium.org/downloads'
+            raise RuntimeError(ns+'\n'+dl)
+        
         driver.get(f'https://genius.com/artists/{self.name}')
-
+        
+        try:
+            driver.find_element_by_class_name('render_404-headline')
+            raise AttributeError(f'{self.name} was not found - did you spell it correctly?')
+        except Exception:
+            pass
+        
         class_name = 'full_width_button.u-clickable.u-quarter_top_margin'
         driver.find_element_by_class_name(class_name).click()
-
+    
         # wait for the page to load
-        time.sleep(.5)
-
-        # click the modal, and scroll to load all albums
         modal_name = 'act-show.cont-artists.snarly.u-noscroll.u-dark_overlay'
-        modal = driver.find_element_by_class_name(modal_name)
+        
+        modal = WebDriverWait(driver, 60).until(
+            presence_of_element_located((By.CLASS_NAME, modal_name))
+        )
+        
+        # click the modal, and scroll to load all albums
+        time.sleep(.5)
         modal.click()
         modal.send_keys(Keys.END)
-        time.sleep(.5)
+        time.sleep(.2)
+        modal.send_keys(Keys.END)
+        time.sleep(.2)
 
         # return html and close connection
         source = driver.page_source
+        driver.close()
         driver.quit()
 
         # initialize soup object and find all albums
         soup = BeautifulSoup(source, features='lxml')
-        modal = soup.find_all('mini-album-card')
+        albums = soup.find_all('mini-album-card')
 
         # function to help out for getting links below
         def get_link(x):
@@ -65,49 +105,48 @@ class Artist:
         
         def get_title(x):
             x = x.find('div', class_='mini_card-title')
-            x = x.get_text()
-            return x
+            return x.get_text()
         
         def get_year(x):
             x = x.find('div', class_='mini_card-subtitle')
-            x = x.get_text()
-            return x
+            return x.get_text()
+        
+        artist = soup.find('h1', 'profile_identity-name_iq_and_role_icon')
+        artist = artist.text.strip().split('\n')[0]
     
         # return paths to all albums
-        album_links = [get_link(x) for x in modal]
-        titles = [get_title(x) for x in modal]
-        year = [get_year(x) for x in modal]
+        album_links = [get_link(x) for x in albums]
+        titles = [get_title(x) for x in albums]
+        year = [get_year(x) for x in albums]
         
         zipped = zip(titles, year, album_links)
         columns = ['albums', 'release_year', 'links']
         albums = pd.DataFrame(zipped, columns=columns)
+        albums['artist'] = artist
         
         self.albums = albums
     
     
-    def get_track_list(self, link):
+    def _song_helper(self, album, verbose=True):
         
         '''
         Return all tracks from an artist's album
         Optimally used inside .get_songs() to return for all albums
         
         '''
-        
+
+        if verbose:
+            print(f'finding album {album.albums}')
+
         # helper fuction for clean track names
         def song_names(string):
             string = string.get_text()
-            string = string.split('\n')[2].strip()
-            return string
+            return string.split('\n')[2].strip()
         
         # make request and create soup object
-        request = requests.get(link)
+        request = requests.get(album.links)
         soup = BeautifulSoup(request.content, features='lxml')
-        
-        # find the album title
-        alb_text = 'header_with_cover_art-primary_info-title header_with_cover_art-primary_info-title--white'
-        album = soup.find('h1', class_=alb_text).get_text()
-        print(f'finding {album}')
-        
+                
         # find the list of songs in album
         songs = soup.find_all('a', class_='u-display_block')
         links = [song['href'] for song in songs]
@@ -117,12 +156,12 @@ class Artist:
         # create df containing names, links, and albums
         columns = ['names', 'links']
         new_df = pd.DataFrame(zipped, columns=columns)
-        new_df['album'] = album
+        new_df['album'] = album.albums
+        new_df['artist'] = album.artist
         return new_df
     
     
     def get_songs(self, end=None):
-    
         '''
         input: dataframe of albums
         output: dataframe of all songs
@@ -130,24 +169,44 @@ class Artist:
         if type(self.albums) != pd.DataFrame:
             raise NotImplementedError('You need to run .get_albums first')
         
-        # create empty df
-        df = pd.DataFrame()
-        
         # create list of the unique album names
-        iterable = self.albums.links[:end]
+        iterable = [x for i,x in self.albums.iloc[:end].iterrows()]
         
-        # 
-        with Pool() as pool:
-            lst = pool.map(self.get_track_list, iterable=iterable)
+        # multiprocessing ftw
+        pool = mp.Pool(5)
+        album_lst = pool.map(self._song_helper, iterable=iterable)
         pool.close()
+        pool.join()
         
         # create df 
-        for x in lst:
-            df = df.append(x)
+        df = pd.concat([album for album in album_lst])
 
         self.songs = df
+
         
-    def get_lyrics(self, alert_pct=20, verbose=True):
+    def _lyric_helper(self, row):
+        
+        request = requests.get(row.links)
+
+        # turn the content into a BeautifulSoup object
+        soup = BeautifulSoup(request.content, features='lxml')
+
+        # find the lyrics and clean
+        lyric_list = soup.find_all('div', class_='Lyrics__Container-sc-1ynbvzw-7')
+        song_lyrics = ' '.join([x.get_text(';') for x in lyric_list if x.text])
+        clean_lyrics = re.sub(r'\[(.*?)\];', "", song_lyrics)
+
+        lyrics = {
+            'artist': row.artist,
+            'album': row.album,
+            'song_title': row.names,
+            'lyrics': clean_lyrics
+        }
+
+        return lyrics
+        
+
+    def get_lyrics(self, end=None):
 
         '''
         input: list of lyric paths
@@ -156,84 +215,34 @@ class Artist:
         '''
         if type(self.songs) != pd.DataFrame:
             raise NotImplementedError('You need to run .get_songs first')
+    
+        iterable = [x for i,x in self.songs.iloc[:end].iterrows()]
         
-        song_links = self.songs.links
-
-        # to print out the retrieval % completion of songs
-        if verbose == True:
-            num_links = len(song_links)
-            alert = round((alert_pct/100) * num_links, 0)
-            alert_num = [i for i in range(num_links) if not i % alert and i !=0]
-
-        # set up and get all lyrics from songs
-        lyrics = []
-
-        for i, path in enumerate(song_links):
-
-            # let user know where the scraping is
-            if verbose == True:
-                if i in alert_num:
-                    print(f'Completed scraping {round((i/num_links)*100, 0)}% of songs.')
-
-            request = requests.get(path)
-
-            # Check to make sure the request didn't return a 404
-            if request.status_code != 200:
-                print(f'Error code: {request.status_code}')
-                print(f'Completed {(i/num_links)*100}% of scraping')
-                song_df = pd.DataFrame(lyrics)
-                return song_df
-
-            # turn the content into a BeautifulSoup object
-            soup = BeautifulSoup(request.content, features='lxml')
-
-            # find the lyrics and clean
-            song_lyrics = soup.find('div', class_='lyrics').get_text()
-            song_lyrics = song_lyrics.split('\n')
-            song_lyrics = [line for line in song_lyrics if not line.startswith('[')]
-            song_lyrics = [line for line in song_lyrics if len(line)]
-
-            # find the song's title
-            song_title = soup.find('h1', class_='header_with_cover_art-primary_info-title')
-            song_title = song_title.get_text()
-
-            # find the artist of the song
-            artist = soup.find('a', class_='header_with_cover_art-primary_info-primary_artist')
-            artist = artist.get_text()
-
-            # find the song's album
-            album = soup.find('a', class_='song_album-info-title').get_text()
-            album = album.split('\n')[1].strip()
-
-            # return all 
-            lyrics.append({'artist': artist, 'album': album, 'song_title': song_title, 'lyrics': song_lyrics})
-        
-        lyric_df = pd.DataFrame(lyrics)
+        # multiprocessing ftw
+        pool = mp.Pool(5)
+        lyric_lst = pool.map(self._lyric_helper, iterable=iterable)
+        pool.close()
+        pool.join()
+    
+        lyric_df = pd.DataFrame(lyric_lst)
         
         self.lyrics = lyric_df
         
         
     def get_sentiment(self):
         
-        lyric_df = self.lyrics
+        df = self.lyrics
         
-        lyric_df['num_lines'] = lyric_df['lyrics'].map(lambda x: len(x))
+        df['num_words'] = df.lyrics.str.split(' ').str.len()
 
-        lyric_df = lyric_df[lyric_df['num_lines'] > 1]
-
-        def get_words(lines):
-            words = [word.lower() for word in lines]
-            words = ' '.join(words)
-            return words
-
-        lyric_df['words'] = lyric_df['lyrics'].map(lambda x: get_words(x))
+        df['words'] = df.lyrics.str.lower()
 
         def get_sentiment(lyrics):
             blob = TextBlob(lyrics)
-            sentiment = blob.sentiment.polarity
-            return sentiment
+            return blob.sentiment
 
-        # calculate sentiment    
-        lyric_df['sentiment'] = lyric_df['words'].map(lambda x: get_sentiment(x))
+        # calculate sentiment
+        _ = df['words'].map(lambda x: get_sentiment(x))
+        df[['polarity', 'subjectivity']]  = pd.DataFrame(_.tolist(),index=df.index)
         
-        self.lyrics = lyric_df
+        self.lyrics = df
